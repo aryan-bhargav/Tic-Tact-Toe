9e543e331ee80df1032fe2dcc8d85a58f7c0027a
aryan-bhargav <bhargavaryan5@gmail.com>
2024-12-26 14:50:02 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import Board from './Components/Board'
+function App() {
+  
+  return (
+    <>
+    <Board/>
+    </>
+  )
+}
+
+export default App
